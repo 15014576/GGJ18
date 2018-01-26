c14dc97695558ae073481e257e75568db8dfbafb
Harold T Spencer <htspencer88@gmail.com>
2018-01-26 21:41:32 +0000

    Movement Basics

---
+++ Assets/_Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 rightVector = Vector3.right * horz;
+        Vector3 forwardVector = Vector3.forward * vert;
+
+        if (Mathf.Abs(rb.velocity.x)/rb.velocity.x != Mathf.Abs(horz)/horz)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+        }
+        /*
+        if (Mathf.Abs(rb.velocity.z) / rb.velocity.z != Mathf.Abs(horz) / horz)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+        }*/
+
+        rb.AddForce((rightVector + forwardVector) * speed);
+        
+		
+	}
+}
