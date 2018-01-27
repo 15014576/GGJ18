725041a1d20cf0112de4b88491dcff403f5e31cf
JSH96 <15014576@students.southwales.ac.uk>
2018-01-27 19:54:45 +0000

    rumour of the day fix

--- Assets/_Scripts/SO Scripts/RumoursForTheDay.cs
+++ Assets/_Scripts/SO Scripts/RumoursForTheDay.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
 [CreateAssetMenu (menuName = "Misc/RumoursForDay")]
-=======
-[CreateAssetMenu(fileName = "RumoursForTheDay")]
->>>>>>> db921c1cd39e7ad6c0dcdeaafea7ca5f33f8d8f2
 public class RumoursForTheDay : ScriptableObject
 {
     public List<Rumour> rumours;
