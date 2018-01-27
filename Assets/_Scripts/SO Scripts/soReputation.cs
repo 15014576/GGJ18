2ff71b74d9a9cb130391fb13eed0dc8f6846534c
JSH96 <15014576@students.southwales.ac.uk>
2018-01-27 12:26:34 +0000

    fuck u start()

---
+++ Assets/_Scripts/SO Scripts/soReputation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "soReputation")]
+public class soReputation : ScriptableObject {
+
+    public int reputation;
+
+    private void OnEnable()
+    {
+        reputation = 0;
+    }
+}
---
+++ Assets/_Scripts/SliderController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SliderController : MonoBehaviour {
+
+    public soReputation update;
+    public Slider repBar;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        Debug.Log("Rep is: " + update.reputation);
+        repBar.value = update.reputation;
+    }
+}
