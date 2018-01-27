75bac56c56a5ab3fe808c58a6243df50abe0458c
Harold T Spencer <htspencer88@gmail.com>
2018-01-27 16:53:49 +0000

    Rumour Mill and Rumour Gen in Group

---
+++ Assets/_Scripts/SO Scripts/RumourPool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RumourPool : ScriptableObject
+{
+    List<Rumour> tierone;
+    List<Rumour> tiertwo;
+    List<Rumour> tierthree;
+
+    public Rumour GetRumour(int tier)
+    {
+        List<Rumour> rumourList;
+
+        switch(tier)
+        {
+            case 1:
+                rumourList = tierone;
+                break;
+            case 2:
+                rumourList = tiertwo;
+                break;
+            case 3:
+                rumourList = tierthree;
+                break;
+            default:
+                Debug.LogWarning("You are asking for a tier that doesn't exist in the RumourPool Class");
+                rumourList = new List<Rumour>();
+                break;
+        }
+
+        return rumourList[Random.Range(0, rumourList.Count)];
+    }
+
+}
---
+++ Assets/_Scripts/SO Scripts/soStature.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "soStature")]
+public class soStature : ScriptableObject
+{
+
+    private int stature;
+
+    public int min;
+    public int max;
+
+    private void OnEnable()
+    {
+        stature = 0;
+    }
+
+    public int getStature()
+    {
+        return stature;
+    }
+
+    public void changeStature(int val)
+    {
+        stature += val;
+
+        if (stature < min)
+        {
+            stature = min;
+        }
+
+        if (stature > max)
+        {
+            stature = max;
+        }
+    }
+}
+
