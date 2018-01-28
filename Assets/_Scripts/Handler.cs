714b4a919d0567b6b6458ad68be8f64a6dc52e0e
Harold T Spencer <htspencer88@gmail.com>
2018-01-28 12:30:29 +0000

    Sorting out States

--- Assets/_Scripts/Handler.cs
+++ Assets/_Scripts/Handler.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Handler : MonoBehaviour {
+public class Handler : MonoBehaviour
+{
+    public State StateManager;
+    public GameObject BrowserCanvas;
+    public GameObject StoryCanvas;
+
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        StateManager.Setup(BrowserCanvas, StoryCanvas);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
--- Assets/_Scripts/SO Scripts/State.cs
+++ Assets/_Scripts/SO Scripts/State.cs
@@ -7,9 +7,49 @@ public class State : ScriptableObject
 {
     public StateName currentState;
 
-    public void Setup()
-    {
+    GameObject Broswer;
+    GameObject Story;
 
+    public void Setup(GameObject _browser, GameObject _story)
+    {
+        Broswer = _browser;
+        Story = _story;
+
+        Broswer.SetActive(false);
+        Story.SetActive(false);
+
+        changeState(StateName.Home);
+    }
+
+    void changeState(StateName newState)
+    {
+        switch(currentState)
+        {
+            case StateName.Home:
+                Broswer.SetActive(false);
+                break;
+            case StateName.Rumours:
+                Story.SetActive(false);
+                break;
+
+        }
+
+        currentState = newState;
+
+        switch(currentState)
+        {
+            case StateName.Home:
+                Broswer.SetActive(true);
+                break;
+            case StateName.Rumours:
+                Story.SetActive(true);
+                break;
+        }
+    }
+
+    public void LogOff()
+    {
+        changeState(StateName.Rumours);
     }
 
 }
