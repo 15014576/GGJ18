2d395b678cee5648067f305b8ce0ed5390c9992b
Harold T Spencer <htspencer88@gmail.com>
2018-01-28 13:21:23 +0000

    Outcomes for the day asset creation

--- Assets/_Scripts/SO Scripts/OutcomesForTheDay.cs
+++ Assets/_Scripts/SO Scripts/OutcomesForTheDay.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
-[CreateAssetMenu(menuName = "Misc/OutcomesForDay")]
-=======
+
 [CreateAssetMenu( menuName = "Misc/Outcomes for the Day")]
->>>>>>> harry
 public class OutcomesForTheDay : ScriptableObject
 {
     public List<PanelInfo> outcomes;
