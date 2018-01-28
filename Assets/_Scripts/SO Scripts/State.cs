9012e6b6cd60b493698185cea5d97f61ccf43b07
Harold T Spencer <htspencer88@gmail.com>
2018-01-28 12:14:46 +0000

    Starting to Prefab everything and introduce handler/holder

---
+++ Assets/_Scripts/SO Scripts/State.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Misc/State")]
+public class State : ScriptableObject
+{
+    public StateName currentState;
+
+    public void Setup()
+    {
+
+    }
+
+}
+public enum StateName
+{
+    Menu, Home, Rumours, Schoolyard, Outcomes
+}
